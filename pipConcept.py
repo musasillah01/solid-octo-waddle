c85692c5be2f788b0a75ffcb90d51c73487c5b89
Musa <sillahm550@gmail.com>
2022-10-28 05:10:11 +0000

    ready to commit

---
+++ pipConcept.py
@@ -0,0 +1,16 @@
+'''[
+# to download django we need a package installer [pip]
+# [pip freeze command] to get the list of python packages in my computer
+
+# in a python projecct, there is a requirement.txt that contains the dependencies the project needs to run. 
+
+# to install flask we do [pip install/uninstall flask] 
+# to use the requirement.txt file, [pip install -r requirements.txt] to install the dependencies.
+
+# creating a virtual environment for windows OS
+# [pip install virtualenv]. to use we create a virtual environment in the django project folder
+# we create a virtual environment using the command [virtualenv simple_django_project] with simple_django_project being the name of the virtual environment
+# to activate the environment we use the command [simple_django_project\Scripts\activate]
+# python installer packages [PIP]
+]
+'''
